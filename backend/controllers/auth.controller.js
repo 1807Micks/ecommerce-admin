d4df76860ec6a31ebaa100b00ca945540e46610a
Your Name <1807micks@gmail.com>
2026-04-10 10:14:12 +0200

    add in first route and controller

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const login = async(req, res)=> {
+    res.send('Login in route called')
+}
+export const logout = async(req, res)=> {
+    res.send('Logout in route called')
+}
+export const signup = async(req, res)=> {
+    res.send('Signup in route called')
+}
