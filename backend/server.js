44821ccd5b6086986be0ef4553b4787bc8f2aaec
Your Name <1807micks@gmail.com>
2026-04-13 13:49:18 +0200

    create admin  Route, protectRoute and products model

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import authRoutes from './routes/auth.route.js'
 import { connectDB } from './lib/db.js'
 import cookieParser from 'cookie-parser'
+
+import authRoutes from './routes/auth.route.js'
+import productRoutes from './routes/product.route.js'
 
 dotenv.config()
 
@@ -11,7 +13,9 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use("/api/auth" , authRoutes)
+app.use("/api/products", productRoutes)
 
 app.listen(PORT, () => {
     console.log('Server is running on http://localhost:' + PORT)
