26edc99f7c8da8542ce6de9d8c7e339b9b5a8d1c
Your Name <1807micks@gmail.com>
2026-04-13 14:29:14 +0200

    create product route for featured products

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,5 +1,5 @@
 import Product from "../models/product.model.js"
-
+import {redis} from "../lib/redis.js"
 
 export const getAllProducts = async(req, res) => {
     try {
@@ -9,5 +9,27 @@ export const getAllProducts = async(req, res) => {
         console.log("Error in getAllProducts", error.message)
         res.status(500).json({message: "Server error", error: error.message})
         
+    }
+}
+
+export const getFeaturedProducts = async(req, res) => {
+    try {
+      let featuredProducts = await redis.get("featuredProcuts")
+      if(featuredProducts){
+        return res.json({products: JSON.parse(featuredProducts)})
+      }
+
+      //if not in redis, fetch from mongodb
+      //.lean() returns plain js objects instead of mongoose documents, improving performance
+      featuredProducts = await Product.find({isFeatured: true}).lean()
+
+      if(!featuredProducts){
+        return res.status(404).json({message: "No featured products found"})
+      }
+      //Store in redis for future quisk access
+      await redis.set("featuredProducts", JSON.stringify(featuredProducts)) //expire in 1 hour
+    } catch (error) {
+        console.log("Error in getFeaturedProducts", error.message)
+        res.status(500).json({message: "Server error", error: error.message})
     }
 }
