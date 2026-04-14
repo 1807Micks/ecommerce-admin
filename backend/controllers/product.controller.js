13efffa6ef1fb440930b5d7cdf56150601cd9724
Your Name <1807micks@gmail.com>
2026-04-14 09:27:17 +0200

    get product by category

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -83,9 +83,20 @@ export const getRecommendedProducts = async(req, res) => {
             }
         }
     ])
-    res.json(products)
+    res.json({products: recommendedProducts})
 } catch (error) {
         console.log("Error in getRecommendedProducts", error.message)
         res.status(500).json({message: "Server error", error: error.message})
+}
+}
+
+export const getProductsByCategory = async(req, res) =>{
+const {category} = req.params
+    try {
+    const products = await Product.find({category})
+    res.json(products)
+} catch (error) {
+    console.log("Error in getProductsByCategory", error.message)
+    res.status(500).json({message: "Server error", error: error.message})
 }
 }
