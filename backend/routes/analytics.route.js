534980ac2dece995397ea8a309bee2034acfb84b
Your Name <1807micks@gmail.com>
2026-04-16 13:19:35 +0200

    analytics

--- backend/routes/analytics.route.js
+++ backend/routes/analytics.route.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js"
-import { getDailySalesData } from "../controllers/analytics.controller.js"
+import { getDailySalesData, getAnalyticsData } from "../controllers/analytics.controller.js"
+
 const router =express.Router()
 
 router.get("/", protectRoute, adminRoute, async(req, res) => {
