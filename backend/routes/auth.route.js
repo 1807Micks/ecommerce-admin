712c9626798f1530eed78054f7581418cf4ab206
Your Name <1807micks@gmail.com>
2026-04-16 13:32:11 +0200

    create profile route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { login, logout, signup, refreshToken } from '../controllers/auth.controller.js'
+import { login, logout, signup, refreshToken, getProfile } from '../controllers/auth.controller.js'
+import protectedRoute from "../middleware/auth.middleware.js"
 
 const router = express.Router()
 
@@ -7,5 +8,6 @@ router.post('/login', login)
 router.post('/signup', signup)
 router.post('/logout', logout)
 router.post("/refreshToken", refreshToken)
+router.get("/profile", protectedRoute, getProfile)
 
 export default router
