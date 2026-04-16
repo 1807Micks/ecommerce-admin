d80d48a1936eb4d7127d57768fefb6a50879e77f
Your Name <1807micks@gmail.com>
2026-04-16 14:54:18 +0200

    create first pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/signup" element={<SignUpPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
